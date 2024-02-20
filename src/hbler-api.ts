6c7f07bfdeddab31e15b72f18a87f39948370372
hugobler <hugo.ernesto@ouribank.com.br>
2024-02-20 18:15:55 -0300

    remove unused function

--- src/hbler-api.ts
+++ src/hbler-api.ts
@@ -1,6 +1,3 @@
 import { listProducts, getProduct } from "./functions";
 
-// Anything exported from this file is importable by other in-browser modules.
-export function publicApiFunction() {}
-
 export { listProducts, getProduct };
